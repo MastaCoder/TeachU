1a9d5fb2cde77aba6ea587dcd4c61c41a6d39369
Rob Liu <therealrobophant@gmail.com>
2019-03-01 20:29:27 -0500

    new nlp library

--- nlu_module (old).py
+++ nlu_module (old).py
@@ -1,18 +1,9 @@
 import requests
 
-
 url = 'https://gateway-wdc.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-11-16'
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 api_key = ('apikey', '-x6m_vef22iFpYejuFHnolIJsDOB0eQUtTUqMRAdsJLY')
-data = """{
-  "url": "http://newsroom.ibm.com/Guerbet-and-IBM-Watson-Health-Announce-Strategic-Partnership-for-Artificial-Intelligence-in-Medical-Imaging-Liver",
-    "features": {
-        "sentiment": {},
-        "categories": {},
-        "concepts": {},
-        "entities": {},
-        "keywords": {}
-    }
-}"""
+with open("data.json") as file:
+    data = file.read()
 r = requests.post(url, data=data, headers=headers, auth=api_key)
 print(r.text)
