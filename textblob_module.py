e55b58a117ac8a96611dabff2d0e1879480de2b4
Rob Liu <therealrobophant@gmail.com>
2019-03-01 21:52:32 -0500

    Update textblob_module.py

--- textblob_module.py
+++ textblob_module.py
@@ -141,28 +141,6 @@ def genQuestion(line):
         print('\n', 'Question: ' + question)
 
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Hello, world!')
-
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        self.send_response(200)
-        self.end_headers()
-        response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)
-        self.wfile.write(response.getvalue())
-
-
-httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
-httpd.serve_forever()
-
 def main():
     """
     Accepts a text file as an argument and generates questions from it.
