c44c8c67d15d0e13277bbdaf4cebfdb4f4333a68
Rob Liu <therealrobophant@gmail.com>
2019-03-01 23:01:02 -0500

    teachu_module begun (custom nlp parsing algorithm)

--- teachu_module.py
+++ teachu_module.py
@@ -1,16 +1,45 @@
-import nltk
-from textblob import Word
 from textblob import TextBlob
-import sys
+
+NOUNS = ["NN", "NNS", "NNP", "NNPS"
+NNPS]
+
+class Sentence:
+    def __init__(self, sentence):
+        self.sentence = sentence
+        self.tags = sentence.tags
+        self.pos = 0
+        self.process()
+
+    def process(self):
+        while True:
+            tag = self.tags[self.pos]
+            if tag[1] == "IN":
+                claux = self.claux()
+                # auxillary clause haha get it
+
+    def claux(self):
+        claux_list = []
+        end_check = False
+        while True:
+            tag = self.tags[self.pos]
+            if tag[1] in NOUNS:
+                if end_check:
+                    return claux_list
+                end_check = True
+            else:
+                end_check = False
+            claux_list.append(tag)
+            self.pos += 1
 
 
-def parse(string):
+def parse_sentences(string):
     txt = TextBlob(string)
     for sentence in txt.sentences:
-        print(sentence)
+        s_map = Sentence(sentence)
 
 
-with open('notes', encoding="utf8") as file:
+with open('notes2', encoding="utf8") as file:
     notes = file.read()
 
-parse(notes)
+parse_sentences(notes)
+
